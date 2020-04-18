e17f5904d5e803dd62dc8222de7f0f7ac79e0067
Daniel Baucom <loudambiance@gmail.com>
2020-04-17 23:44:15 -0400

    Adding bStats and metrics.

--- src/main/java/com/chumcraft/usefulwanderingtrader/UWTPlugin.java
+++ src/main/java/com/chumcraft/usefulwanderingtrader/UWTPlugin.java
@@ -17,6 +17,7 @@
 
 package com.chumcraft.usefulwanderingtrader;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UWTPlugin extends JavaPlugin {
@@ -35,21 +36,49 @@ public class UWTPlugin extends JavaPlugin {
         this.config = new Configuration(this);
         this.playerheads = new PlayerHeads(this);
         this.miniblocks = new Miniblocks(this);
+        this.updateMetrics();
 
         // Commands enabled with following method must have entries in plugin.yml
         getServer().getPluginManager().registerEvents(new WanderingTraderListener(this), this);
     }
 
-    public Configuration getConfiguration()
-    {
+    public Configuration getConfiguration() {
         return this.config;
     }
 
-    public PlayerHeads getPlayerHeads(){
+    public PlayerHeads getPlayerHeads() {
         return this.playerheads;
     }
 
-    public Miniblocks getMiniblocks(){
+    public Miniblocks getMiniblocks() {
         return this.miniblocks;
+    }
+
+    private void updateMetrics() {
+        int bStatID = 7211;
+        Metrics metrics = new Metrics(this, bStatID);
+
+        // Optional: Add custom charts
+        String playerheads = Integer.toString(this.playerheads.PlayerHeadList.size());
+        String headprice = Integer.toString(this.config.getIntSetting("heads", "price"));
+        String headmax = Integer.toString(this.config.getIntSetting("heads", "max"));
+        String headmin = Integer.toString(this.config.getIntSetting("heads", "min"));
+        String headenabled = Boolean.toString(this.config.getBooleanSetting("heads", "enabled"));
+        String extraheadsenabled = Boolean.toString(this.config.getBooleanSetting("heads", "extraheads"));
+        String miniblockprice = Integer.toString(this.config.getIntSetting("miniblocks", "price"));
+        String miniblockmax = Integer.toString(this.config.getIntSetting("miniblocks", "max"));
+        String miniblockmin = Integer.toString(this.config.getIntSetting("miniblocks", "min"));
+        String miniblockenabled = Boolean.toString(this.config.getBooleanSetting("miniblocks", "enabled"));
+        metrics.addCustomChart(new Metrics.SimplePie("player_heads", () -> playerheads));
+        metrics.addCustomChart(new Metrics.SimplePie("head_price", () -> headprice));
+        metrics.addCustomChart(new Metrics.SimplePie("head_max", () -> headmax));
+        metrics.addCustomChart(new Metrics.SimplePie("head_min", () -> headmin));
+        metrics.addCustomChart(new Metrics.SimplePie("heads_enabled", () -> headenabled));
+        metrics.addCustomChart(new Metrics.SimplePie("extra_heads_enabled", () -> extraheadsenabled));
+        metrics.addCustomChart(new Metrics.SimplePie("miniblock_price", () -> miniblockprice));
+        metrics.addCustomChart(new Metrics.SimplePie("miniblock_max", () -> miniblockmax));
+        metrics.addCustomChart(new Metrics.SimplePie("miniblock_min", () -> miniblockmin));
+        metrics.addCustomChart(new Metrics.SimplePie("miniblocks_enabled", () -> miniblockenabled));
+
     }
 }
